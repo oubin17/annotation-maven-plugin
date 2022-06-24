085e3684f7237ba82e885d8cec3de951f0a48b68
oubin <oubin.ob@antgroup.com>
2022-06-24 16:10:48 +0800

    mojo

---
+++ src/main/java/org/example/DemoMojo.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+/**
+ * DemoMojo
+ *
+ * @author oubin.ob
+ * @version : DemoMojo.java v 0.1 2022/6/24 15:47 oubin.ob Exp $$
+ */
+@Mojo(name = "hello")
+public class DemoMojo extends AbstractMojo {
+
+    @Parameter(name = "name", defaultValue = "kiwi")
+    private String name;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("hello" + name);
+
+    }
+}
